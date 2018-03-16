fd740bb896f1a6ba7166a3b914c35cb7a3fe1a70
Milos Vasic <Milos.Vasic@voiceworks.com>
2018-03-16 15:39:20 +0100

    Apache Factory: More flow added.

--- factory.py
+++ factory.py
@@ -8,10 +8,19 @@ apacheTarBz = "httpd-2.4.29.tar.gz"
 apacheDownload = "http://www-us.apache.org/dist//httpd/" + apacheTarBz
 
 steps = [
+    "clear",
+    "echo 'Installing dependencies'"
     'yum install group -y "Development Tools"',
+    "clear",
+    "echo 'Making Apache home directory'",
     "mkdir " + apacheHome,
+    "echo 'Downloading Apache'",
     "wget " + apacheDownload + " -P " + apacheHome + "/",
-    "tar  -xvzf " + apacheHome + "/" + apacheTarBz + " --directory " + home
+    "clear",
+    "echo 'Extracting Apache'",
+    "tar  -xvzf " + apacheHome + "/" + apacheTarBz + " --directory " + home,
+    "clear",
+    "echo 'Apache installation extracted'"
 ]
 
 for step in steps:
