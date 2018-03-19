a2bb4debb13e96387ca8f886a3442495c1d212f5
Milos Vasic <Milos.Vasic@voiceworks.com>
2018-03-19 14:00:32 +0100

    Apache Factory: Refactoring, cleaning up.

---
+++ commands.py
@@ -0,0 +1,62 @@
+import os
+from os.path import expanduser
+
+home = expanduser("~")
+
+apacheHome = home + "/Apache2"
+apacheTarBz = "httpd-2.4.29.tar.gz"
+apacheDownload = "http://www-us.apache.org/dist//httpd/" + apacheTarBz
+
+
+def get_su(what):
+    return "su -c '" + what + "'"
+
+
+def get_yum(what):
+    return "yum install -y " + what
+
+
+def get_yum_group(what):
+    return "yum install group -y '" + what + "'"
+
+
+def concatenate(*what):
+    result = ""
+    for item in what:
+        result += " " + item + ";"
+
+
+def echo(*what):
+    result = ""
+    for item in what:
+        result += item + '\n'
+    return "echo '" + result + "'"
+
+
+def clear():
+    return "clear"
+
+
+def wget(what, **params):
+    destination = 'destination'
+    if destination in params:
+        return wget(what) + " -P " + params[destination]
+    else:
+        return "wget " + what
+
+
+def mkdir(dir_name):
+    return "mkdir " + dir_name
+
+
+def extract(what, **params):
+    destination = 'destination'
+    if destination in params:
+        return "tar  -xvzf " + what + " --directory " + params[destination]
+    else:
+        return "tar  -xvzf " + what
+
+
+def run(*what):
+    for cmd in what:
+        os.system(cmd)
--- factory.py
+++ factory.py
@@ -1,61 +1,4 @@
-import os
-from os.path import expanduser
-
-home = expanduser("~")
-
-apacheHome = home + "/Apache2"
-apacheTarBz = "httpd-2.4.29.tar.gz"
-apacheDownload = "http://www-us.apache.org/dist//httpd/" + apacheTarBz
-
-
-def get_su(what):
-    return "su -c '" + what + "'"
-
-
-def get_yum(what):
-    return "yum install -y " + what
-
-
-def get_yum_group(what):
-    return "yum install group -y '" + what + "'"
-
-
-def concatenate(*what):
-    result = ""
-    for item in what:
-        result += " " + item + ";"
-
-
-def echo(*what):
-    result = ""
-    for item in what:
-        result += item + '\n'
-    return "echo '" + result + "'"
-
-
-def clear():
-    return "clear"
-
-
-def wget(what, **params):
-    destination = 'destination'
-    if destination in params:
-        return wget(what) + " -P " + params[destination]
-    else:
-        return "wget " + what
-
-
-def mkdir(dir_name):
-    return "mkdir " + dir_name
-
-
-def extract(what, **params):
-    destination = 'destination'
-    if destination in params:
-        return "tar  -xvzf " + what + " --directory " + params[destination]
-    else:
-        return "tar  -xvzf " + what
-
+from commands import *
 
 steps = [
     clear(),
@@ -87,5 +30,4 @@ steps = [
     echo("Making Apache build")
 ]
 
-for step in steps:
-    os.system(step)
+run(steps)
