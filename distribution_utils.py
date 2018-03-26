7efec43acccf4757dfbb8ee77d4aab68f4fc3654
Milos Vasic <milos85vasic@gmail.com>
2018-03-26 20:11:19 +0200

    Apache Factory: Check if file exists.

--- distribution_utils.py
+++ distribution_utils.py
@@ -1,3 +1,4 @@
+import os.path
 from configuration import *
 
 
@@ -5,11 +6,15 @@ def get_port():
     port = "8080"
     prefix = "Listen "
     path = apache_conf + "/" + httpd_conf
-    with open(path, "rt") as fin:
-        for line in fin:
-            print line
-            if prefix in line:
-                port = line.replace(prefix, "")
-                print "Apache will listen at port: " + port
-                return str(port)
+    if os.path.exists(path):
+        print "Parsing for port number: " + path
+        with open(path, "rt") as fin:
+            for line in fin:
+                print line
+                if prefix in line:
+                    port = line.replace(prefix, "")
+                    print "Apache will listen at port: " + port
+                    return str(port)
+    else:
+        print "There is no file to parse for port information: " + path
     return port
