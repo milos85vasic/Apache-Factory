547a255089aa05ff199f526ac0195ea55a57b2d0
Milos Vasic <milos85vasic@gmail.com>
2018-03-26 19:54:51 +0200

    Apache Factory: Port parsing update.

--- distribution_utils.py
+++ distribution_utils.py
@@ -2,13 +2,15 @@ from configuration import *
 
 
 def get_port():
+    port = "8080"
     prefix = "Listen "
     path = apache_conf + "/" + httpd_conf
     print "Parsing for port number: " + path
     with open(path, "rt") as fin:
         for line in fin:
+            print line
             if prefix in line:
                 port = line.replace(prefix, "")
                 print "Apache will listen at port: " + port
                 return str(port)
-    return "8080"
+    return port
