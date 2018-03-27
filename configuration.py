d6f764fd0c3a6b706c49c12cdb4d0babc202d441
Milos Vasic <milos85vasic@gmail.com>
2018-03-27 22:47:53 +0200

    Apache Factory: Path set to '/user/share'.

--- configuration.py
+++ configuration.py
@@ -19,7 +19,7 @@ httpd_conf_matrix_home_dir_placeholder = "APACHE_FACTORY_HOMEDIR"         # http
 httpd_conf_matrix_port_placeholder = "APACHE_FACTORY_PORT"                # httpd.conf matrix port placeholder.
 
 # JSON file name used tha will be used by tool to store the data.
-default_configuration_json = "global_configuration.json"
+default_configuration_json = "/usr/share/global_configuration.json"
 
 # Repository containing default Apache conf:
 configuration_repository = "https://github.com/milos85vasic/Apache-Factory-Config-Default.git"
