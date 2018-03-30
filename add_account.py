bef1e8da362cb86f8b514f95494a24133dae931f
Milos Vasic <milos85vasic@gmail.com>
2018-03-30 19:55:13 +0200

    Apache Factory: Add to group.

--- add_account.py
+++ add_account.py
@@ -16,6 +16,7 @@ steps = [
             add_user(account),
             passwd(account),
             add_group(apache_factory_group),
+            add_to_group(account, apache_factory_group),
             mkdir(apache_factory_configuration_dir),
             chmod(apache_factory_configuration_dir, "770"),
             chgrp(apache_factory_group, apache_factory_configuration_dir),
