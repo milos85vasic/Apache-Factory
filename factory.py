ac9742d85a714fb858c0d1745747a725330d4858
Milos Vasic <milos85vasic@gmail.com>
2018-04-09 16:17:53 +0200

    Apache Factory: FIXME added.

--- factory.py
+++ factory.py
@@ -24,7 +24,7 @@ steps = [
                     clear(),
                     mkdir(apache_home),
                     mkdir(content_dir_path),
-                    cp(content_dir_matrix_path, content_dir_path),
+                    cp(content_dir_matrix_path, content_dir_path),  # FIXME: It does not copy the file.
                     wget(apache_download, destination=(home + "/")),
                     clear(),
                     extract(apache_extract, destination=home),
