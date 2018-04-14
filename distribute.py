3ca5269090bb1b1c45bd81906e4103304b809520
Milos Vasic <milos85vasic@gmail.com>
2018-04-14 17:16:23 +0200

    Apache Factory: Server admin.

--- distribute.py
+++ distribute.py
@@ -28,9 +28,10 @@ steps = [
         apache_conf + "/" + httpd_conf_matrix,
         apache_conf + "/" + httpd_conf,
         httpd_conf_matrix_home_dir_placeholder, home,
-        httpd_conf_matrix_port_placeholder, str(system_configuration["port"]),
+        httpd_conf_matrix_port_placeholder, str(system_configuration[key_configuration_port]),
         httpd_conf_matrix_user_placeholder, account,
-        httpd_conf_matrix_group_placeholder, account
+        httpd_conf_matrix_group_placeholder, account,
+        httpd_conf_matrix_server_admin_placeholder, str(system_configuration[key_configuration_server_admin])
     ),
     rm(
         apache_conf + "/" + httpd_conf_matrix
