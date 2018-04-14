6ca6254eaf606cbd1c5c361eaf1309ba7cf4a4da
Milos Vasic <milos85vasic@gmail.com>
2018-04-14 15:13:53 +0200

    Apache Factory: Fixing and polishing.

--- add_account.py
+++ add_account.py
@@ -12,38 +12,37 @@ for arg in sys.argv:
 set_git_info()
 git_configuration = get_git_info()
 
-steps = [
-    run_as_su(
-        concatenate(
-            cd("~"),
-            clear(),
-            add_user(account),
-            passwd(account),
-            add_group(apache_factory_group),
-            add_to_group(account, apache_factory_group),
-            mkdir(apache_factory_configuration_dir),
-            chmod(apache_factory_configuration_dir, "770"),
-            chgrp(apache_factory_group, apache_factory_configuration_dir),
-            cd(get_home_directory_path(account)),
-            mkdir(apache_factory),
-            git_clone(git_configuration[key_repository]),
-            cd(apache_factory),
-            git_checkout(git_configuration[key_branch]),
-            # cd(".."),
-            # chown(account, get_home_directory_path(account)),
-            # chgrp(account, get_home_directory_path(account)),
-            # chmod(get_home_directory_path(account), "750"),
-            # cd("~"),
-            # clear()
-        )
-    )
-    # ,
-    # run_as_user(
-    #     account,
-    #     concatenate(
-    #         python(factory_script)
-    #     )
-    # )
-]
-
-run(steps)
+# steps = [
+#     run_as_su(
+#         concatenate(
+#             cd("~"),
+#             clear(),
+#             add_user(account),
+#             passwd(account),
+#             add_group(apache_factory_group),
+#             add_to_group(account, apache_factory_group),
+#             mkdir(apache_factory_configuration_dir),
+#             chmod(apache_factory_configuration_dir, "770"),
+#             chgrp(apache_factory_group, apache_factory_configuration_dir),
+#             cd(get_home_directory_path(account)),
+#             mkdir(apache_factory),
+#             git_clone(git_configuration[key_repository]),
+#             cd(apache_factory),
+#             git_checkout(git_configuration[key_branch]),
+#             cd(".."),
+#             chown(account, get_home_directory_path(account)),
+#             chgrp(account, get_home_directory_path(account)),
+#             chmod(get_home_directory_path(account), "750"),
+#             cd("~"),
+#             clear()
+#         )
+#     ),
+#     run_as_user(
+#         account,
+#         concatenate(
+#             python(factory_script)
+#         )
+#     )
+# ]
+#
+# run(steps)
