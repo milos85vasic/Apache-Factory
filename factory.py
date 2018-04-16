828df18f2ecb0dafb6b932413137e3ff64a74324
Milos Vasic <milos85vasic@gmail.com>
2018-04-16 10:04:43 +0200

    Apache Factory: Testing, fixing, polishing - DONE.

--- factory.py
+++ factory.py
@@ -13,38 +13,38 @@ steps = [
     clear(),
     run_as_su(
         concatenate(
-            # get_yum_group("Development Tools"),
-            # get_yum("openssl-devel"),
-            # get_yum("gcc"),
-            # get_yum("make"),
-            # get_yum("cmake"),
-            # get_yum("apr-devel"),
-            # get_yum("apr-util-devel"),
-            # get_yum("wget"),
-            # get_yum("git"),
+            get_yum_group("Development Tools"),
+            get_yum("openssl-devel"),
+            get_yum("gcc"),
+            get_yum("make"),
+            get_yum("cmake"),
+            get_yum("apr-devel"),
+            get_yum("apr-util-devel"),
+            get_yum("wget"),
+            get_yum("git"),
             add_to_group(account, apache_factory_group),
             run_as_user(
                 account,
                 concatenate(
                     clear(),
                     cd("~"),
-                    # mkdir(apache_home),
-                    # mkdir(content_dir_path(user_home())),
-                    # cp(
-                    #     content_dir_matrix_path(user_home()),
-                    #     content_dir_path(user_home())
-                    # ),
-                    # wget(apache_download, destination=(home + "/")),
-                    # clear(),
-                    # extract(apache_extract, destination=home),
-                    # clear(),
-                    # cd(apache_extracted),
-                    # "./configure --prefix=" + apache_home,
-                    # "make",
-                    # "make install",
-                    # cd("~"),
-                    # clear(),
-                    # rm(apache_extracted),
+                    mkdir(apache_home),
+                    mkdir(content_dir_path(user_home())),
+                    cp(
+                        content_dir_matrix_path(user_home()),
+                        content_dir_path(user_home())
+                    ),
+                    wget(apache_download, destination=(home + "/")),
+                    clear(),
+                    extract(apache_extract, destination=home),
+                    clear(),
+                    cd(apache_extracted),
+                    "./configure --prefix=" + apache_home,
+                    "make",
+                    "make install",
+                    cd("~"),
+                    clear(),
+                    rm(apache_extracted),
                     mkdir(brotli),
                     cd(brotli),
                     git_clone_to(brotli_repository, "./"),
@@ -55,15 +55,14 @@ steps = [
                 )
             ),
             cd(get_home_directory_path(account) + "/" + brotli + "/out"),
-            "pwd",
             "make install",
-            # run_as_user(
-            #     account,
-            #     concatenate(
-            #         cd(user_home() + "/" + apache_factory),
-            #         python(distribution_script)
-            #     )
-            # )
+            run_as_user(
+                account,
+                concatenate(
+                    cd(user_home() + "/" + apache_factory),
+                    python(distribution_script)
+                )
+            )
         )
     )
 ]
