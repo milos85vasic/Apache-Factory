afde2b1450bc0186395f6eadce96e119e4ce27bb
Milos Vasic <milos85vasic@gmail.com>
2018-04-16 09:40:29 +0200

    Apache Factory: Prevent execution if account already exists.

--- add_account.py
+++ add_account.py
@@ -1,4 +1,5 @@
 import sys
+import pwd
 from commands import *
 from system_configuration import *
 from configuration import *
@@ -9,34 +10,38 @@ git_configuration = get_git_info()
 system_configuration = init_system_configuration(sys.argv)
 account = get_account()
 
-steps = [
-    run_as_su(
-        concatenate(
-            cd("~"),
-            clear(),
-            add_user(account),
-            passwd(account),
-            add_group(apache_factory_group),
-            add_to_group(account, apache_factory_group),
-            cd(get_home_directory_path(account)),
-            mkdir(apache_factory),
-            git_clone(git_configuration[key_repository]),
-            cd(apache_factory),
-            git_checkout(git_configuration[key_branch]),
-            cd(".."),
-            chown(account, get_home_directory_path(account)),
-            chgrp(account, get_home_directory_path(account)),
-            chmod(get_home_directory_path(account), "750"),
-            cd("~"),
-            clear()
+try:
+    pwd.getpwnam(account)
+    print("Account already exists: " + account)
+except KeyError:
+    steps = [
+        run_as_su(
+            concatenate(
+                cd("~"),
+                clear(),
+                add_user(account),
+                passwd(account),
+                add_group(apache_factory_group),
+                add_to_group(account, apache_factory_group),
+                cd(get_home_directory_path(account)),
+                mkdir(apache_factory),
+                git_clone(git_configuration[key_repository]),
+                cd(apache_factory),
+                git_checkout(git_configuration[key_branch]),
+                cd(".."),
+                chown(account, get_home_directory_path(account)),
+                chgrp(account, get_home_directory_path(account)),
+                chmod(get_home_directory_path(account), "750"),
+                cd("~"),
+                clear()
+            )
+        ),
+        run_as_user(
+            account,
+            concatenate(
+                python(factory_script)
+            )
         )
-    ),
-    run_as_user(
-        account,
-        concatenate(
-            python(factory_script)
-        )
-    )
-]
+    ]
 
-run(steps)
+    run(steps)
