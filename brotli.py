35ba44c3a45b0e2e868e9be3b63659a628a97504
Milos Vasic <milos85vasic@gmail.com>
2018-04-16 13:45:28 +0200

    Apache Factory: Testing, fixing, polishing.

--- brotli.py
+++ brotli.py
@@ -1,24 +1,31 @@
 from commands import *
 from configuration import *
+from system_configuration import *
+
+account = get_account()
 
 steps = [
-    cd(home),
-    mkdir(brotli),
-    cd(brotli),
-    git_clone_to(brotli_repository, "./"),
-    "mkdir out && cd out",
-    "../configure-cmake",
-    "make",
-    "make test",
-    "make install",
-    cd("~"),
-    mkdir(brotli_module),
-    cd(brotli_module),
-    "git clone --depth=1 --recursive " + brotli_module_repository + " ./",
-    "./autogen.sh",
-    "./configure",
-    "make",
-    "install -p -m 755 -D .libs/mod_brotli.so " + apache_home + "/modules/mod_brotli.so"
+    concatenate(
+        cd(get_home_directory_path(account)),
+        mkdir(brotli),
+        cd(brotli),
+        git_clone_to(brotli_repository, "./"),
+        "mkdir out && cd out",
+        "../configure-cmake",
+        "make",
+        "make test",
+        "make install"
+    ),
+    concatenate(
+        cd(get_home_directory_path(account)),
+        mkdir(brotli_module),
+        cd(brotli_module),
+        "git clone --depth=1 --recursive " + brotli_module_repository + " ./",
+        "./autogen.sh",
+        "./configure",
+        "make",
+        "install -p -m 755 -D .libs/mod_brotli.so " + apache_home + "/modules/mod_brotli.so"
+    )
 ]
 
 run(steps)
