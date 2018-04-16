3dee8ceebc637eac72d9ab49e2f4c36618f7c0c4
Milos Vasic <milos85vasic@gmail.com>
2018-04-16 14:28:16 +0200

    Apache Factory: Building PHP.

--- factory.py
+++ factory.py
@@ -13,54 +13,55 @@ steps = [
     clear(),
     run_as_su(
         concatenate(
-            get_yum_group("Development Tools"),
-            get_yum("openssl-devel"),
-            get_yum("gcc"),
-            get_yum("make"),
-            get_yum("cmake"),
-            get_yum("automake"),
-            get_yum("libtool"),
-            get_yum("apr-devel"),
-            get_yum("apr-util-devel"),
-            get_yum("wget"),
-            get_yum("git"),
-            get_yum("httpd-devel"),
+            # get_yum_group("Development Tools"),
+            # get_yum("openssl-devel"),
+            # get_yum("gcc"),
+            # get_yum("make"),
+            # get_yum("cmake"),
+            # get_yum("automake"),
+            # get_yum("libtool"),
+            # get_yum("apr-devel"),
+            # get_yum("apr-util-devel"),
+            # get_yum("wget"),
+            # get_yum("git"),
+            # get_yum("httpd-devel"),
             add_to_group(account, apache_factory_group),
-            run_as_user(
-                account,
-                concatenate(
-                    clear(),
-                    cd("~"),
-                    mkdir(apache_home),
-                    mkdir(content_dir_path(user_home())),
-                    cp(
-                        content_dir_matrix_path(user_home()),
-                        content_dir_path(user_home())
-                    ),
-                    wget(apache_download, destination=(home + "/")),
-                    clear(),
-                    extract(apache_extract, destination=home),
-                    clear(),
-                    cd(apache_extracted),
-                    "./configure --prefix=" + apache_home,
-                    "make",
-                    "make install",
-                    cd("~"),
-                    clear(),
-                    rm(apache_extracted)
-                )
-            ),
+            # run_as_user(
+            #     account,
+            #     concatenate(
+            #         clear(),
+            #         cd("~"),
+            #         mkdir(apache_home),
+            #         mkdir(content_dir_path(user_home())),
+            #         cp(
+            #             content_dir_matrix_path(user_home()),
+            #             content_dir_path(user_home())
+            #         ),
+            #         wget(apache_download, destination=(home + "/")),
+            #         clear(),
+            #         extract(apache_extract, destination=home),
+            #         clear(),
+            #         cd(apache_extracted),
+            #         "./configure --prefix=" + apache_home,
+            #         "make",
+            #         "make install",
+            #         cd("~"),
+            #         clear(),
+            #         rm(apache_extracted)
+            #     )
+            # ),
+            # cd(home + "/" + apache_factory),
+            # python(brotli_installation_script, account),
             cd(home + "/" + apache_factory),
-            python(brotli_installation_script, account),
-            cd(home + "/" + apache_factory),
-            python(php_installation_script, account),
-            run_as_user(
-                account,
-                concatenate(
-                    cd(user_home() + "/" + apache_factory),
-                    python(distribution_script)
-                )
-            )
+            python(php_installation_script, account)
+            # ,
+            # run_as_user(
+            #     account,
+            #     concatenate(
+            #         cd(user_home() + "/" + apache_factory),
+            #         python(distribution_script)
+            #     )
+            # )
         )
     )
 ]
