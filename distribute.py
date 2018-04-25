a6fd1736bd4ee21adbb366714c23297222f65083
Milos Vasic <milos85vasic@gmail.com>
2018-04-25 14:12:33 +0200

    Apache Factory: Distributing services.

--- distribute.py
+++ distribute.py
@@ -14,42 +14,42 @@ save_system_configuration(system_configuration)
 
 steps = [
     cd("~"),
-    clear(),
-    rm(apache_conf),
-    mkdir(apache_conf),
-    chmod(apache_conf, "755"),
-    concatenate(
-        cd(apache_conf),
-        git_clone_into(configuration_repository, here),
-        cd("~")
-    ),
-    clear(),
-    python(
-        wipe_script,
-        apache_conf + "/" + httpd_conf_matrix,
-        apache_conf + "/" + httpd_conf,
-        httpd_conf_matrix_home_dir_placeholder, home,
-        httpd_conf_matrix_port_placeholder, str(system_configuration[key_configuration_port]),
-        httpd_conf_matrix_user_placeholder, account,
-        httpd_conf_matrix_group_placeholder, account,
-        httpd_conf_matrix_server_admin_placeholder, str(system_configuration[account][key_configuration_server_admin])
-    ),
-    rm(
-        apache_conf + "/" + httpd_conf_matrix
-    ),
+    # clear(),
+    # rm(apache_conf),
+    # mkdir(apache_conf),
+    # chmod(apache_conf, "755"),
+    # concatenate(
+    #     cd(apache_conf),
+    #     git_clone_into(configuration_repository, here),
+    #     cd("~")
+    # ),
+    # clear(),
+    # python(
+    #     wipe_script,
+    #     apache_conf + "/" + httpd_conf_matrix,
+    #     apache_conf + "/" + httpd_conf,
+    #     httpd_conf_matrix_home_dir_placeholder, home,
+    #     httpd_conf_matrix_port_placeholder, str(system_configuration[key_configuration_port]),
+    #     httpd_conf_matrix_user_placeholder, account,
+    #     httpd_conf_matrix_group_placeholder, account,
+    #     httpd_conf_matrix_server_admin_placeholder, str(system_configuration[account][key_configuration_server_admin])
+    # ),
+    # rm(
+    #     apache_conf + "/" + httpd_conf_matrix
+    # ),
     concatenate(
         cd(content_dir_path(get_home_directory_path(account))),
         mkdirs(get_services_directories()),
         cd("~")
     ),
-    concatenate(
-        cd(apache_bin),
-        apache_start(),
-        sleep(10),
-        cd("~"),
-    ),
-    clear(),
-    curl("http://localhost:" + str(system_configuration["port"]))
+    # concatenate(
+    #     cd(apache_bin),
+    #     apache_start(),
+    #     sleep(10),
+    #     cd("~"),
+    # ),
+    # clear(),
+    # curl("http://localhost:" + str(system_configuration["port"]))
 ]
 
 run(steps)
