a399c414cfb32a3a4bee3cfff6cd6fb45ed31797
Milos Vasic <milos85vasic@gmail.com>
2018-04-30 09:54:58 +0200

    Apache Factory: Apache factory starter.

---
+++ starter.py
@@ -0,0 +1,20 @@
+import sys
+from commands import *
+from system_configuration import *
+from configuration import *
+from git_info import *
+
+system_configuration = get_system_configuration()
+
+for item in system_configuration.keys():
+    account = item
+    script = get_home_directory_path(account)
+    script += "/" + apache2 + "/bin/apachectl"
+    if os.path.isfile(script):
+        steps = [
+            run_as_user(
+                python(script, "start")
+            )
+        ]
+
+        run(steps)
