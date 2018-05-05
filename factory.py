7fc89e725b819018e2e7569a00fdbf6a337f4c20
Milos Vasic <milos85vasic@gmail.com>
2018-05-05 17:41:39 +0200

    Apache Factory: working on MySQL support.

--- factory.py
+++ factory.py
@@ -29,36 +29,36 @@ steps = [
             mkdir(content_dir_path(user_home())),
             chown(account, content_dir_path(user_home())),
             chgrp(account, content_dir_path(user_home())),
-            # run_as_user(
-            #     account,
-            #     concatenate(
-            #         clear(),
-            #         cd("~"),
-            #         mkdir(apache_home),
-            #         mkdir(apache_home + "/" + apache_vhosts_directory),
-            #         mkdir(php_home),
-            #         cp(
-            #             content_dir_matrix_path(user_home()),
-            #             content_dir_path(user_home())
-            #         ),
-            #         cp(
-            #             content_dir_matrix_path_php(user_home()),
-            #             content_dir_path(user_home())
-            #         ),
-            #         wget(apache_download, destination=(home + "/")),
-            #         clear(),
-            #         extract(apache_extract, destination=home),
-            #         clear(),
-            #         cd(apache_extracted),
-            #         "./configure --prefix=" + apache_home,
-            #         "make",
-            #         "make install",
-            #         cd("~"),
-            #         clear(),
-            #         rm(apache_extracted)
-            #     )
-            # ),
-            # cd(home + "/" + apache_factory),
+            run_as_user(
+                account,
+                concatenate(
+                    clear(),
+                    cd("~"),
+                    mkdir(apache_home),
+                    mkdir(apache_home + "/" + apache_vhosts_directory),
+                    mkdir(php_home),
+                    cp(
+                        content_dir_matrix_path(user_home()),
+                        content_dir_path(user_home())
+                    ),
+                    cp(
+                        content_dir_matrix_path_php(user_home()),
+                        content_dir_path(user_home())
+                    ),
+                    wget(apache_download, destination=(home + "/")),
+                    clear(),
+                    extract(apache_extract, destination=home),
+                    clear(),
+                    cd(apache_extracted),
+                    "./configure --prefix=" + apache_home,
+                    "make",
+                    "make install",
+                    cd("~"),
+                    clear(),
+                    rm(apache_extracted)
+                )
+            ),
+            cd(home + "/" + apache_factory),
             # python(brotli_installation_script, account),
             run_as_user(
                 account,
