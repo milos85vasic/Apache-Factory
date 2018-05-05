a7a5b849ec0abc4037ad0603a8fced049fe902d1
Milos Vasic <milos85vasic@gmail.com>
2018-05-05 18:24:28 +0200

    Apache Factory: Working on MySQL support.

--- php.py
+++ php.py
@@ -10,6 +10,22 @@ def user_home():
     return get_home_directory_path(account)
 
 
+system_configuration = get_system_configuration()
+
+features = None
+
+if key_services in system_configuration[account]:
+    if key_features in system_configuration[account][key_services]:
+        features = system_configuration[account][key_services][key_features]
+else:
+    features = [key_feature_mysql]
+
+configure = "./configure --prefix=" + user_home() + \
+            "/" + php + " --with-apxs2=" + user_home() + "/" + apache2 + "/bin/apxs"
+
+if features and key_feature_mysql in features:
+    configure += " --with-mysql=" + user_home() + "/" + mysql
+
 steps = [
     concatenate(
         cd(user_home()),
@@ -18,7 +34,7 @@ steps = [
         extract(user_home() + "/" + php_tar_gz, destination=user_home()),
         clear(),
         cd(php_tar_gz.replace(".tar.gz", "")),
-        "./configure --prefix=" + user_home() + "/" + php + " --with-apxs2=" + user_home() + "/" + apache2 + "/bin/apxs" + " --with-mysql",
+        configure,
         "make",
         "make install",
         cp("php.ini-development", user_home() + "/" + php + "/lib/php.ini"),
