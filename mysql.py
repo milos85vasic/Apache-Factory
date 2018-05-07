cbdd30f5fc237015733bc044266e826690a055ea
Milos Vasic <milos85vasic@gmail.com>
2018-05-07 15:34:40 +0200

    Apache Factory: Working on MySQL support.

--- mysql.py
+++ mysql.py
@@ -31,14 +31,14 @@ if has_feature(account, feature_mysql):
             mkdir(mysql + "/" + mysql_pid_dir),
             mkdir(mysql + "/" + mysql_share_dir),
             mkdir(mysql + "/" + mysql_conf_dir),
+
+            # TODO: Git clone and wipe my.conf.matrix
+
             wget(mysql_download, destination=(user_home() + "/")),
             clear(),
             extract(user_home() + "/" + mysql_tar_gz, destination=user_home()),
             clear(),
             cd(mysql_tar_gz.replace(".tar.gz", "")),
-
-            # TODO: Git clone and wipe my.conf.matrix
-
             "cmake ./ -DDOWNLOAD_BOOST=1 -DWITH_BOOST=" + get_home_directory_path(account) + "/Boost",
             "make",
             'make install DESTDIR="' + user_home() + "/" + mysql + "/" + mysql_installation_dir + '"',
