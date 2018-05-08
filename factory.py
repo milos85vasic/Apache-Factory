32bc74032729774c0064d114414b784aca33ac40
Milos Vasic <milos85vasic@gmail.com>
2018-05-08 13:11:39 +0200

    Apache Factory: MySQL feature first run: Testing and fixing.

--- factory.py
+++ factory.py
@@ -38,41 +38,41 @@ steps = [
                 concatenate(
                     clear(),
                     cd("~"),
-                    mkdir(apache_home),
-                    mkdir(apache_home + "/" + apache_vhosts_directory),
-                    mkdir(php_home),
+                    # mkdir(apache_home),
+                    # mkdir(apache_home + "/" + apache_vhosts_directory),
+                    # mkdir(php_home),
                     mkdir(mysql_home),
-                    cp(
-                        content_dir_matrix_path(user_home()),
-                        content_dir_path(user_home())
-                    ),
-                    cp(
-                        content_dir_matrix_path_php(user_home()),
-                        content_dir_path(user_home())
-                    ),
-                    wget(apache_download, destination=(home + "/")),
-                    clear(),
-                    extract(apache_extract, destination=home),
-                    clear(),
-                    cd(apache_extracted),
-                    "./configure --prefix=" + apache_home,
-                    "make",
-                    "make install",
-                    cd("~"),
-                    clear(),
-                    rm(apache_extracted),
-                    rm(apache_tar_gz)
+                    # cp(
+                    #     content_dir_matrix_path(user_home()),
+                    #     content_dir_path(user_home())
+                    # ),
+                    # cp(
+                    #     content_dir_matrix_path_php(user_home()),
+                    #     content_dir_path(user_home())
+                    # ),
+                    # wget(apache_download, destination=(home + "/")),
+                    # clear(),
+                    # extract(apache_extract, destination=home),
+                    # clear(),
+                    # cd(apache_extracted),
+                    # "./configure --prefix=" + apache_home,
+                    # "make",
+                    # "make install",
+                    # cd("~"),
+                    # clear(),
+                    # rm(apache_extracted),
+                    # rm(apache_tar_gz)
                 )
             ),
             cd(home + "/" + apache_factory),
-            python(brotli_installation_script, account),
+            # python(brotli_installation_script, account),
             run_as_user(
                 account,
                 concatenate(
                     cd(user_home() + "/" + apache_factory),
                     python(mysql_installation_script, account),
-                    python(php_installation_script, account),
-                    python(distribution_script)
+                    # python(php_installation_script, account),
+                    # python(distribution_script)
                 )
             )
         )
