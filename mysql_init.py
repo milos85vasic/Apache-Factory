b65656d9a6a66bb135690013fadf0d0165b924a3
Milos Vasic <milos85vasic@gmail.com>
2018-05-22 16:44:25 +0200

    Apache Factory: Working on MySQL implementation - catch the output.

--- mysql_init.py
+++ mysql_init.py
@@ -6,7 +6,7 @@ from mysql_common import *
 
 command = get_mysql_bin_directory() + initialize
 
-result = subprocess.check_output(command.split(" "), stderr=subprocess.DEVNULL)
+result = subprocess.check_output(command.split(" "))
 
 print(result)
 print("- - - - - - - - - - - -")
