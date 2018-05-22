e7b42da192fdbc612e3374830712677e318f8c44
Milos Vasic <milos85vasic@gmail.com>
2018-05-22 17:31:27 +0200

    Apache Factory: Working on MySQL implementation - catch the output.

--- mysql_init.py
+++ mysql_init.py
@@ -2,25 +2,16 @@ import os
 import sys
 from configuration import *
 from mysql_common import *
-from subprocess import Popen, PIPE, STDOUT
 
 command = get_mysql_bin_directory() + initialize
 
-p = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate()
+steps = [
+    command
+]
 
-print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-print(command)
-print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-print(stdout)
-print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-print(stderr)
-print("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
+run(command)
+
 
 # TODO: parse  /home/test1/MySQL/Log/error.log for password.
 
-# if os.path.isfile(mysql_initialization_tmp):
-#     os.remove(mysql_initialization_tmp)
-# else:
-#     with open(mysql_initialization_tmp, "wt") as fout:
-#         fout.write(result)
+
