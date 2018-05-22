80394d81669098e039e097681e5488a15b35b94b
Milos Vasic <milos85vasic@gmail.com>
2018-05-22 16:52:33 +0200

    Apache Factory: Working on MySQL implementation - catch the output.

--- mysql_init.py
+++ mysql_init.py
@@ -3,16 +3,23 @@ import subprocess
 import sys
 from configuration import *
 from mysql_common import *
+from subprocess import Popen, PIPE
 
 command = get_mysql_bin_directory() + initialize
 
-result = subprocess.check_output(command.split(" "))
+# result = subprocess.check_output(command.split(" "))
 
-print(result)
+
+p = Popen(command.split(" "), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+stdout, stderr = p.communicate()
+
+print(stdout)
+print("- - - - - - - - - - - -")
+print(stderr)
 print("- - - - - - - - - - - -")
 
-if os.path.isfile(mysql_initialization_tmp):
-    os.remove(mysql_initialization_tmp)
-else:
-    with open(mysql_initialization_tmp, "wt") as fout:
-        fout.write(result)
+# if os.path.isfile(mysql_initialization_tmp):
+#     os.remove(mysql_initialization_tmp)
+# else:
+#     with open(mysql_initialization_tmp, "wt") as fout:
+#         fout.write(result)
