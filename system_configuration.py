d49e2014320a315904d55f979144470ea8323c59
Milos Vasic <milos85vasic@gmail.com>
2018-05-23 17:15:04 +0200

    Apache Factory: Working on MySQL implementation.

--- system_configuration.py
+++ system_configuration.py
@@ -105,6 +105,7 @@ def get_services_directories(account):
 
 
 def has_feature(account, feature):
+    print("ACCOUNT: " + account)
     features = None
     system_configuration = get_system_configuration()
 
