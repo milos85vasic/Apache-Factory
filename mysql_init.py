69e179971f0619ec74d6c989b7b1eb7f7dfb8bcb
Milos Vasic <milos85vasic@gmail.com>
2018-05-26 17:54:02 +0200

    Apache Factory: Working on MySQL implementation.

--- mysql_init.py
+++ mysql_init.py
@@ -2,6 +2,8 @@ import os
 import sys
 from configuration import *
 from mysql_common import *
+import secrets
+import string
 
 command = get_mysql_bin_directory() + initialize
 
@@ -11,31 +13,26 @@ steps = [
 
 run(steps)
 
-contains = "A temporary password is generated for root@localhost: "
-path = get_mysql_logs_directory() + "/" + "error.log"
-with open(path) as fp:
-    for cnt, line in enumerate(fp):
-        if contains in line:
-            split_result = line.split(contains)
-            mysql_password = split_result[1].strip()
-            print("MySQL root user password: " + mysql_password)
-            system_configuration[account][key_services][key_credentials] = {feature_mysql: mysql_password}
-            save_system_configuration(system_configuration)
+alphabet = string.ascii_letters + string.digits
+mysql_password = ''.join(secrets.choice(alphabet) for i in range(20))
 
-            alter_user = "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"  # TODO: Create users and databases.
+print("MySQL root user password: " + mysql_password)
+system_configuration[account][key_services][key_credentials] = {feature_mysql: mysql_password}
+save_system_configuration(system_configuration)
 
-            steps = [
-                output(alter_user, mysql_init_tmp),
-                get_mysql_bin_directory() + get_start_command_init(user_home()),
-                python(
-                    killer_script,
-                    "mysql"
-                ),
+alter_user = "ALTER USER 'root'@'localhost' IDENTIFIED BY '" + mysql_password + "';"
 
-                # TODO: start cmd
-                # get_mysql_bin_directory() + get_start_command(user_home())
+steps = [
+    output(alter_user, mysql_init_tmp),
+    get_mysql_bin_directory() + get_start_command_init(user_home()),
+    python(
+        killer_script,
+        "mysql"
+    ),
 
-                # TODO: remove *.tmp
-            ]
-            run(steps)
-            break
+    # TODO: start cmd
+    # get_mysql_bin_directory() + get_start_command(user_home())
+
+    # TODO: remove *.tmp
+]
+run(steps)
