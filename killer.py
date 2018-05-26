f73620c2224d6b002116000a193e65c0f09c3d26
Milos Vasic <milos85vasic@gmail.com>
2018-05-26 18:51:55 +0200

    Apache Factory: Working on MySQL implementation.

--- killer.py
+++ killer.py
@@ -1,4 +1,24 @@
+import subprocess
+import sys
+import re
 
+what = sys.argv[1]
+command = "ps -A | grep " + what
 
-# TODO: Implement me.
+ps = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+result = ps.communicate()[0]
 
+# result = "15639 pts/0    00:00:01 mysqld"
+
+lines = str(result).split("\n")
+
+for line in lines:
+    if line.endswith(what):
+        pid = ""
+        for char in line.strip():
+            if str(char).isdigit():
+                pid += char
+            else:
+                break
+        print(line)
+        print("We are about to kill: " + str(pid))
