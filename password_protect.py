7a78d9924ef908a6598526ac851068395b40daa4
Milos Vasic <milos85vasic@gmail.com>
2018-06-01 21:35:16 +0200

    Apache Factory: Password protect directories - work in progress.

--- password_protect.py
+++ password_protect.py
@@ -8,15 +8,9 @@ account = getpass.getuser()
 
 system_configuration = get_system_configuration()
 
-print(account)
-print(system_configuration)
-
 if account in system_configuration:
-    print("Ok 1")
     if key_services in system_configuration[account]:
-        print("Ok 1")
         if key_password_protect in system_configuration[account][key_services]:
-            print("Ok 2")
             for password_protect in system_configuration[account][key_services][key_password_protect]:
                 print(
                     "We are about to initialize file user with credentials [ " +
