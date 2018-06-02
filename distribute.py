a042163ec904749ca460c20fe1d2517a6224fe17
Milos Vasic <milos85vasic@gmail.com>
2018-06-02 13:05:02 +0200

    Apache Factory: Main proxy - work in progress.

--- distribute.py
+++ distribute.py
@@ -8,16 +8,23 @@ from system_configuration import *
 account = getpass.getuser()
 
 system_configuration = get_system_configuration()
-system_configuration[key_configuration_port] = system_configuration[key_configuration_port] + 1
-system_configuration[account][key_configuration_port] = system_configuration[key_configuration_port]
-
-save_system_configuration(system_configuration)
 
 configuration_repo = configuration_repository
+
+incrementPortNumber = True
 
 if key_configuration in system_configuration:
     if key_configuration_repository in system_configuration[key_configuration]:
         configuration_repo = system_configuration[key_configuration][key_configuration_repository]
+        if key_explicit_port_number in system_configuration[key_configuration]:
+            if system_configuration[key_configuration][key_explicit_port_number]:
+                incrementPortNumber = False
+
+if incrementPortNumber:
+    system_configuration[key_configuration_port] = system_configuration[key_configuration_port] + 1
+    system_configuration[account][key_configuration_port] = system_configuration[key_configuration_port]
+
+save_system_configuration(system_configuration)
 
 steps = [
     cd("~"),
