2be3e2bf96896663e9c463d63afd2de040a50d39
Milos Vasic <milos85vasic@gmail.com>
2018-06-14 14:16:55 +0200

    Apache Factory: PHP configuration - work in progress.

--- php.py
+++ php.py
@@ -18,28 +18,51 @@ configure = "./configure --prefix=" + user_home() + \
 if has_feature(account, feature_mysql):
     configure += " --with-mysql=" + user_home() + "/" + mysql + "/" + mysql_installation_dir + "/usr/local/mysql"
 
-steps = [
-    concatenate(
-        cd(user_home()),
-        wget(php_download, destination=(user_home() + "/")),
-        extract(user_home() + "/" + php_tar_gz, destination=user_home()),
-        cd(php_tar_gz.replace(".tar.gz", "")),
-        configure,
-        "make",
-        "make install",
-        cd(user_home()),
-        mkdir(php_conf_php_init_dir),
-        git_clone_to(php_conf_repository, "./" + php_conf_php_init_dir),
-        cd(user_home() + "/" + apache_factory),
-        python(
-            "Toolkit/" + wipe_script,
-            user_home() + "/" + php_conf_php_init_dir + "/php.ini.matrix",
-            user_home() + "/" + php_conf_php_init_dir + "/php.ini"
-        ),
-        cd(user_home()),
-        rm(php_tar_gz),
-        rm(php_tar_gz.replace(".tar.gz", ""))
-    )
-]
+if has_feature(account, feature_mysql):
+    steps = [
+        concatenate(
+            cd(user_home()),
+            wget(php_download, destination=(user_home() + "/")),
+            extract(user_home() + "/" + php_tar_gz, destination=user_home()),
+            cd(php_tar_gz.replace(".tar.gz", "")),
+            configure,
+            "make",
+            "make install",
 
-run(steps)
+            cd(user_home()),
+            mkdir(php_conf_php_init_dir),
+            git_clone_to(php_conf_repository, "./" + php_conf_php_init_dir),
+            cd(user_home() + "/" + apache_factory),
+            python(
+                "Toolkit/" + wipe_script,
+                user_home() + "/" + php_conf_php_init_dir + "/php.ini.matrix",
+                user_home() + "/" + php_conf_php_init_dir + "/php.ini",
+                php_conf_matrix_mysql_socket, user_home() + "/" + mysql + "/" + mysql_sock_dir + "/mysqld.sock"
+            ),
+
+            cd(user_home()),
+            rm(php_tar_gz),
+            rm(php_tar_gz.replace(".tar.gz", ""))
+        )
+    ]
+    run(steps)
+else:
+    steps = [
+        concatenate(
+            cd(user_home()),
+            wget(php_download, destination=(user_home() + "/")),
+            extract(user_home() + "/" + php_tar_gz, destination=user_home()),
+            cd(php_tar_gz.replace(".tar.gz", "")),
+            configure,
+            "make",
+            "make install",
+            cp("php.ini-development", user_home() + "/" + php + "/lib/php.ini"),
+            cd(user_home()),
+            rm(php_tar_gz),
+            rm(php_tar_gz.replace(".tar.gz", ""))
+        )
+    ]
+    run(steps)
+
+
+
