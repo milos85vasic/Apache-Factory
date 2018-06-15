6c27d510d8924ff8c0537fc08434f5907a3307be
Milos Vasic <milos85vasic@gmail.com>
2018-06-15 15:19:22 +0200

    Apache Factory: Pip install alphabet.

--- factory.py
+++ factory.py
@@ -44,6 +44,7 @@ steps = [
             get_yum("libwebp-devel"),
             get_yum("libjpeg-devel"),
             pip_upgrade(),
+            pip("alphabet"),
             add_to_group(account, apache_factory_group),
             mkdir(content_dir_path(user_home())),
             chown(account, content_dir_path(user_home())),
