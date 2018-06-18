be323785ee906fe470e859b9a93d4862a9db5249
Milos Vasic <milos85vasic@gmail.com>
2018-06-18 15:41:57 +0200

    Apache Factory: Script fix.

--- starter.py
+++ starter.py
@@ -41,14 +41,17 @@ for item in system_configuration.keys():
             if key_configuration_port_mysql in system_configuration[account]:
                 port = system_configuration[account][key_configuration_port_mysql]
         mysql_full_path = get_home_directory_path(account) + "/" + mysql + "/"
-        script = mysql_full_path + mysql_bin_dir + "/mysqld --tmpdir=" + mysql_full_path + "tmp --datadir=" + \
-                 mysql_full_path + "data " + "--secure-file-priv=" + mysql_full_path + "priv --port=" + str(port) + \
-                 " --user=" + account + " " + "--socket=" + mysql_full_path + "socket/mysqld.sock &"
+        script = mysql_full_path + mysql_bin_dir + "/mysqld"
 
         if os.path.isfile(script):
             # MySQL 8.0:
             # script += " --defaults-extra-file=" + get_home_directory_path(account) + "/" + mysql + "/" \
             #           + mysql_conf_dir + "/my.cnf &"
+
+            # My SQL 5.5.60:
+            script += " --tmpdir=" + mysql_full_path + "tmp --datadir=" + mysql_full_path + "data " + \
+                      "--secure-file-priv=" + mysql_full_path + "priv --port=" + str(port) + " --user=" + account + \
+                      " " + "--socket=" + mysql_full_path + "socket/mysqld.sock &"
 
             steps = [
                 run_as_user(account, script)
