cccfaa13a15b1191fe98c551bb5a2e7d4842d8e4
Milos Vasic <milos85vasic@gmail.com>
2018-07-17 15:12:11 +0200

    Apache Factory: Certbot fixing and polishing.

--- certbot_renew.py
+++ certbot_renew.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
+import getpass
 import sys
 
 from Toolkit.commands import *
 from Toolkit.system_configuration import *
 from configuration import *
 
+account = getpass.getuser()
 certbot_command = sys.argv[1]
 
+if account != "root":
+    print "Only super user can trigger certbot renew."
+    sys.exit(2)
+
+# TODO: This paths should be relative.
 commands = [
     run_as_su(
         concatenate(
