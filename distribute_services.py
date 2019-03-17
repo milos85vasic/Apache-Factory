a47fe397deb16f43ad14d668d7b6392c347a07df
Milos Vasic <milos85vasic@gmail.com>
2019-03-17 17:47:13 +0100

    Apache Factory: Fixing and polishing.

--- distribute_services.py
+++ distribute_services.py
@@ -72,66 +72,71 @@ if account in system_configuration:
                 urls = None
                 if key_services_urls in service:
                     urls = service[key_services_urls]
-                root = service[key_service_root]
-                logs_home = get_home_directory_path(account) + "/" + apache2 + "/logs"
-                destination_file = vhosts_directory + "/" + url + ".conf"
-                if not os.path.isfile(destination_file):
-                    try:
-                        with open(destination_file, 'w') as outfile:
-                            port = str(system_configuration[account][key_configuration_port])
-                            outfile.write("\n")
-                            outfile.write("<VirtualHost *:" + port + ">")
-                            outfile.write("\n")
-                            outfile.write("\tDocumentRoot " + root)
-                            outfile.write("\n")
-                            outfile.write("\tServerName " + url)
-                            outfile.write("\n")
-                            if urls:
-                                for alias in urls:
-                                    outfile.write("\tServerAlias " + alias)
-                                    outfile.write("\n")
-                            outfile.write("\tErrorLog " + logs_home + "/" + url + ".error.log")
-                            outfile.write("\n")
-                            outfile.write("\tLogFormat " + '"%h %l %u %t \\"%r\\" %>s %b"' + " common")
-                            outfile.write("\n")
-                            outfile.write("\tCustomLog " + logs_home + "/" + url + ".access.log common")
-                            outfile.write("\n")
-                            outfile.write('\t<Directory "' + root + '">')
-                            outfile.write("\n")
-                            outfile.write("\t\tAllowOverride All")
-                            outfile.write("\n")
-                            outfile.write("\t</Directory>")
-                            outfile.write("\n")
 
-                            if key_password_protect in system_configuration[account][key_services]:
-                                key_pp = key_password_protect
-                                for password_protect in system_configuration[account][key_services][key_pp]:
-                                    for directory in password_protect[key_password_protect_directories]:
-                                        if directory[key_password_protect_service] == url:
-                                            outfile.write("\n")
-                                            outfile.write(
-                                                '\t<Directory "' + root + "/" + directory[
-                                                    key_password_protect_path] + '">'
-                                            )
-                                            outfile.write("\n")
-                                            outfile.write("\t\tAuthType Basic")
-                                            outfile.write("\n")
-                                            outfile.write('\t\tAuthName "Restricted Content"')
-                                            outfile.write("\n")
-                                            outfile.write(
-                                                "\t\tAuthUserFile " + get_home_directory_path(
-                                                    account) + "/" + security + "/" +
-                                                passwd_file
-                                            )
-                                            outfile.write("\n")
-                                            outfile.write("\t\tRequire valid-user")
-                                            outfile.write("\n")
-                                            outfile.write("\t</Directory>")
-                                            outfile.write("\n\n")
+                if key_service_root in service:
+                    root = service[key_service_root]
+                    logs_home = get_home_directory_path(account) + "/" + apache2 + "/logs"
+                    destination_file = vhosts_directory + "/" + url + ".conf"
+                    if not os.path.isfile(destination_file):
+                        try:
+                            with open(destination_file, 'w') as outfile:
+                                port = str(system_configuration[account][key_configuration_port])
+                                outfile.write("\n")
+                                outfile.write("<VirtualHost *:" + port + ">")
+                                outfile.write("\n")
+                                outfile.write("\tDocumentRoot " + root)
+                                outfile.write("\n")
+                                outfile.write("\tServerName " + url)
+                                outfile.write("\n")
+                                if urls:
+                                    for alias in urls:
+                                        outfile.write("\tServerAlias " + alias)
+                                        outfile.write("\n")
+                                outfile.write("\tErrorLog " + logs_home + "/" + url + ".error.log")
+                                outfile.write("\n")
+                                outfile.write("\tLogFormat " + '"%h %l %u %t \\"%r\\" %>s %b"' + " common")
+                                outfile.write("\n")
+                                outfile.write("\tCustomLog " + logs_home + "/" + url + ".access.log common")
+                                outfile.write("\n")
+                                outfile.write('\t<Directory "' + root + '">')
+                                outfile.write("\n")
+                                outfile.write("\t\tAllowOverride All")
+                                outfile.write("\n")
+                                outfile.write("\t</Directory>")
+                                outfile.write("\n")
 
-                            outfile.write("</VirtualHost>")
-                    except IOError:
-                        print("Can't access " + destination_file)
+                                if key_password_protect in system_configuration[account][key_services]:
+                                    key_pp = key_password_protect
+                                    for password_protect in system_configuration[account][key_services][key_pp]:
+                                        for directory in password_protect[key_password_protect_directories]:
+                                            if directory[key_password_protect_service] == url:
+                                                outfile.write("\n")
+                                                outfile.write(
+                                                    '\t<Directory "' + root + "/" + directory[
+                                                        key_password_protect_path] + '">'
+                                                )
+                                                outfile.write("\n")
+                                                outfile.write("\t\tAuthType Basic")
+                                                outfile.write("\n")
+                                                outfile.write('\t\tAuthName "Restricted Content"')
+                                                outfile.write("\n")
+                                                outfile.write(
+                                                    "\t\tAuthUserFile " + get_home_directory_path(
+                                                        account) + "/" + security + "/" +
+                                                    passwd_file
+                                                )
+                                                outfile.write("\n")
+                                                outfile.write("\t\tRequire valid-user")
+                                                outfile.write("\n")
+                                                outfile.write("\t</Directory>")
+                                                outfile.write("\n\n")
+
+                                outfile.write("</VirtualHost>")
+                        except IOError:
+                            print("Can't access " + destination_file)
+                else:
+                    print("No root for: " + url)
+
 
 system_configuration = get_system_configuration()
 if account in system_configuration:
